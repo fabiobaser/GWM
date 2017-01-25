8bdabb100b687322c189111f99e388b9bd1807dc
Fabio Baser <f.baser@me.com>
2017-01-25 13:46:21 +0100

    Video auch auf Mobile

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -140,7 +140,7 @@
             // transform value.
             val: val,
             // initial and halfway/final transform origin.
-            origin: {initial: origin, halfway: origin_2},
+            origin: {initial: origin, halfway: origin_2}
         };
     };
 
@@ -273,13 +273,11 @@ jQuery(document).ready(function ($) {
     if ($('.cd-bg-video-wrapper').length > 0) {
         var videoWrapper = $('.cd-bg-video-wrapper'),
                 mq = window.getComputedStyle(document.querySelector('.cd-bg-video-wrapper'), '::after').getPropertyValue('content').replace(/"/g, "").replace(/'/g, "");
-        if (mq == 'desktop') {
-            // we are not on a mobile device
-            var videoUrl = videoWrapper.data('video'),
-                    video = $('<video loop><source src="' + videoUrl + '.mp4" type="video/mp4" /><source src="' + videoUrl + '.webm" type="video/webm" /></video>');
-            video.appendTo(videoWrapper);
-            video.get(0).play();
-        }
+        // we are not on a mobile device
+        var videoUrl = videoWrapper.data('video'),
+                video = $('<video loop><source src="' + videoUrl + '.mp4" type="video/mp4" /><source src="' + videoUrl + '.webm" type="video/webm" /></video>');
+        video.appendTo(videoWrapper);
+        video.get(0).play();
     }
 
     /*
